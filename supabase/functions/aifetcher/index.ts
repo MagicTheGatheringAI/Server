f874b5f2736dc598cc6c8138aeca45aa31a92018
Josh Phillips <drag0n2ooth230@gmail.com>
2023-05-24 00:14:12 -0600

    clean up imports and add the double query to supa

--- supabase/functions/aifetcher/index.ts
+++ supabase/functions/aifetcher/index.ts
@@ -1,29 +1,13 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
 import { serve } from "http/server.ts";
-import { ChatOpenAI } from "https://esm.sh/langchain/chat_models/openai";
 import { OpenAIEmbeddings } from "https://esm.sh/langchain/embeddings/openai";
-import { LLMChain } from "https://esm.sh/langchain/chains";
-import { CallbackManager } from "https://esm.sh/langchain/callbacks";
-import {
-  ChatPromptTemplate,
-  HumanMessagePromptTemplate,
-} from "https://esm.sh/langchain/prompts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.21";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.22";
 import { SupabaseHybridSearch } from "https://esm.sh/langchain/retrievers/supabase";
-import { SupabaseVectorStore } from "https://esm.sh/langchain/vectorstores/supabase";
 import { OpenAI } from "https://esm.sh/langchain/llms/openai";
-import { loadQAStuffChain, loadQAMapReduceChain } from "https://esm.sh/langchain/chains";
-import { Document } from "https://esm.sh/langchain/document";
+import { loadQAStuffChain, loadQAMapReduceChain, loadQARefineChain } from "https://esm.sh/langchain/chains";
 
 import { corsHeaders } from "../_shared/cors.ts";
 
 const openaikey = "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";
-const prompt = ChatPromptTemplate.fromPromptMessages([
-  HumanMessagePromptTemplate.fromTemplate("{input}"),
-]);
 
 serve(async (req) => {
 
@@ -43,36 +27,49 @@ serve(async (req) => {
 
   const embeddings = new OpenAIEmbeddings({openAIApiKey: openaikey});
   console.log("embeddings is created")
-  const retriever = new SupabaseHybridSearch(embeddings, {
+  const rulesRetriever = new SupabaseHybridSearch(embeddings, {
     client: supabase,
     //  Below are the defaults, expecting that you set up your supabase table and functions according to the guide above. Please change if necessary.
     similarityK: 2,
     keywordK: 2,
-    tableName: "documents",
-    similarityQueryName: "match_documents",
-    keywordQueryName: "kw_match_documents",
+    tableName: "rules",
+    similarityQueryName: "match_rules",
+    keywordQueryName: "kw_match_rules",
+  });
+  const cardsRetriever = new SupabaseHybridSearch(embeddings, {
+    client: supabase,
+    //  Below are the defaults, expecting that you set up your supabase table and functions according to the guide above. Please change if necessary.
+    similarityK: 2,
+    keywordK: 2,
+    tableName: "cards",
+    similarityQueryName: "match_cards",
+    keywordQueryName: "kw_match_cards",
   });
   console.log("retriever is created")
   console.log(JSON.stringify({ input }))
-  
-  const docs = await retriever.getRelevantDocuments(JSON.stringify({ input }));
-  console.log("response is created")
 
-  const llmB = new OpenAI({openAIApiKey: openaikey, maxConcurrency: 10 });
-  const chainB = loadQAMapReduceChain(llmB);
-  const resB = await chainB.call({
-    input_documents: docs,
-    question: JSON.stringify({ input }),
+  const cardsDocs = await cardsRetriever.getRelevantDocuments(JSON.stringify({ input }));
+  const rulesDocs = await rulesRetriever.getRelevantDocuments(JSON.stringify({ input }));
+  console.log("docs are found")
+  console.log(cardsDocs)
+  console.log(rulesDocs)
+
+  const finalDocs = {...cardsDocs, ...rulesDocs}
+  const model = new OpenAI({openAIApiKey: openaikey, maxConcurrency: 10 });
+  const chain = loadQARefineChain(model);
+  const res = await chain.call({
+    input_documents: finalDocs,
+    question: JSON.stringify({input}),
 });
-  return new Response(JSON.stringify(resB), {
+
+  return new Response(JSON.stringify(res), {
   headers: { ...corsHeaders, "Content-Type": "application/json" },
   });
 });
 
 
 
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/aifetcher' \
 //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
 //   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+//   --data '{"input":"tell me how the attack phase works in magic the gathering"}'
