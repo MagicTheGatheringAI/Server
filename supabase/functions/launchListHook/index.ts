8eb0bdc13234025cfeb6f7d35bee9df15d5f3fc9
j1philli <jphilli1996@gmail.com>
2023-08-17 20:32:18 -0600

    add function to sync waitlist entries to supabase

---
+++ supabase/functions/launchListHook/index.ts
@@ -0,0 +1,29 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.32.0'
+
+const supabaseClient = createClient(
+  // Supabase API URL - env var exported by default.
+  Deno.env.get('SUPABASE_URL') ?? '',
+  // Supabase API ANON KEY - env var exported by default.
+  Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+)
+
+serve(async (req) => {
+  const event = await req.json()
+  console.log(event[0].referral_code, event[0].email)
+  const { error } = await supabaseClient
+    .from('waitlist')
+    .insert({ referID: event[0].referral_code, email: event[0].email })
+    if (error) throw error
+
+  return new Response(
+    JSON.stringify("200"),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
