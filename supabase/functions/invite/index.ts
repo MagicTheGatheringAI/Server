7b8ddd5ac5e997e28ff2ae2a460167d8e1a77f6f
j1philli <jphilli1996@gmail.com>
2023-09-04 11:38:49 -0600

    add reply_to to emails

--- supabase/functions/invite/index.ts
+++ supabase/functions/invite/index.ts
@@ -21,6 +21,7 @@ serve(async (req) => {
             from: 'Josh <josh@invites.magicrules.ai>',
             to: [tablechange.record.email],
             subject: "Invite to Magic AI!",
+            reply_to: "josh@magicrules.ai",
             html: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
             <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
             
--- supabase/functions/launchListHook/index.ts
+++ supabase/functions/launchListHook/index.ts
@@ -38,6 +38,7 @@ serve(async (req) => {
         from: 'Josh <josh@invites.magicrules.ai>',
         to: [event[0].email],
         subject: "You are on the waitlist for Magic AI!",
+        reply_to: "josh@magicrules.ai",
         html: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
         
